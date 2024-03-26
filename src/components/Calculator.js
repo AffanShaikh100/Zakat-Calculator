89f6396701f1059baab73222943f6cb64ccca4e5
Affan Shaikh <affanshaikh525@gmail.com>
2024-03-26 18:20:16 +0530

    Adding comment

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -149,6 +149,8 @@ const Calculator = () =>{
         </div>
       </div>
 
+      <p>Disclaimer: For any further clarifications or more details on Zakat calculations, kindly contact your local Imam / Mufti</p>
+
     </div>
   );
 }
