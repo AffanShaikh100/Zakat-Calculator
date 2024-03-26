bd172a1e86d189cb34094bf34d09521a03c14550
Affan Shaikh <affanshaikh525@gmail.com>
2024-03-26 14:52:09 +0530

    Adding footer and some css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,14 @@ import Header from "../components/Header";
 import "./mediaQueries.css";
 import "./index.css";
 import FormInputs from "../components/Form";
+import Footer from "../components/Footer";
 
 export default function HomePage (){
     return(
         <div>
             <Header/>
             <FormInputs/>
+            <Footer/>
         </div>
     )
 }
